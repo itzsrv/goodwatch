6c8ed757aed151a4fd74bd0925f83f76e5248df9
Saurav Singh <itzsrv@outlook.com>
2022-08-17 02:43:47 +0530

    feat: add orchestration to call movieinfo

---
+++ movie-catalog-service/src/main/java/com/goodwatch/moviecatalogservice/models/Movie.java
@@ -0,0 +1,43 @@
+package com.goodwatch.moviecatalogservice.models;
+
+public class Movie {
+    private String movieId;
+    private String movieName;
+    private String movieDesc;
+
+    public Movie() {
+    }
+
+
+    public Movie(String movieId, String movieName, String movieDesc) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.movieDesc = movieDesc;
+    }
+
+
+    public String getMovieId() {
+        return this.movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return this.movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieDesc() {
+        return this.movieDesc;
+    }
+
+    public void setMovieDesc(String movieDesc) {
+        this.movieDesc = movieDesc;
+    }
+
+}
--- movie-catalog-service/src/main/java/com/goodwatch/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/goodwatch/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,19 @@
 package com.goodwatch.moviecatalogservice.resources;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.goodwatch.moviecatalogservice.models.CatalogItem;
+import com.goodwatch.moviecatalogservice.models.Movie;
+import com.goodwatch.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/api/catalog")
@@ -17,17 +23,26 @@ public class MovieCatalogResource{
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         
 
+        RestTemplate restTemplate = new RestTemplate();
+        
         //get all rated movieIds - hardcode
-
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234",3),
+            new Rating("3456", 4)
+        );
 
         //for each movieId, call movieInfo service and get movieInfo
-
+        return ratings.stream().map(rating-> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/api/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getMovieName(), movie.getMovieDesc(), rating.getMovieRating());
+        })
+        .collect(Collectors.toList());
 
         //put them all together
 
 
-        return Collections.singletonList(
-            new CatalogItem("transformers", "animated", 4)
-        );
+        // return Collections.singletonList(
+        //     new CatalogItem("transformers", "animated", 4)
+        // );
     }
 }
