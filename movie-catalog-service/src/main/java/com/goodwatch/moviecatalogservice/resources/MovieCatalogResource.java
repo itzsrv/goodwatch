b2f813831c94da08032e8863b6f536ca4465dbb2
Saurav Singh <itzsrv@outlook.com>
2022-08-17 02:06:13 +0530

    feat: fix moviecatalogservice to run on port 8081

--- movie-catalog-service/src/main/java/com/goodwatch/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/goodwatch/moviecatalogservice/resources/MovieCatalogResource.java
@@ -15,6 +15,17 @@ public class MovieCatalogResource{
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        
+
+        //get all rated movieIds - hardcode
+
+
+        //for each movieId, call movieInfo service and get movieInfo
+
+
+        //put them all together
+
+
         return Collections.singletonList(
             new CatalogItem("transformers", "animated", 4)
         );
