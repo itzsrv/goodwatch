ce857a54ae91c05cd1dad5648519e304896f4aa7
Saurav Singh <itzsrv@outlook.com>
2022-08-18 13:11:02 +0530

    fix: remove unnecessary import in ratingresource

--- ratings-data-service/src/main/java/com/goodwatch/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/com/goodwatch/ratingsdataservice/resources/RatingResource.java
@@ -3,7 +3,6 @@ package com.goodwatch.ratingsdataservice.resources;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
